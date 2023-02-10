165bccab41a92b182cb8a43e04a4288803f09e85
jony wu <jonywu@gmail.com>
2023-02-10 18:05:43 +0800

    [Vue] - 取得 api 資料

--- src/components/03_Layout_Component/Banner.vue
+++ src/components/03_Layout_Component/Banner.vue
@@ -1,6 +1,30 @@
 <!-- 模板用 script -->
 <script>
-  
+import { onMounted } from 'vue';
+import axios from 'axios';
+
+const runkey = 'c911c55c8e4f22c1ef5066573f7f0585';
+export default {
+  setup() {
+    onMounted(() => {
+      axios.get('api/productindex',{
+        header: {
+          runkey: runkey,
+        },
+        params: {
+          category: null,
+          count: 10,
+          page: 1
+        }
+      })
+        .then(response =>{
+          console.log(response.data);
+        })
+    });
+    
+    return {};
+  }
+}
 </script>
 
 
