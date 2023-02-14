c4e46b091f79e44a1241762e4bc3c6d2df46c2e9
jony wu <jonywu@gmail.com>
2023-02-14 18:42:48 +0800

    [ Vue ] - 取得卡片資料並綁定

--- src/components/03_Layout_Component/CardList.vue
+++ src/components/03_Layout_Component/CardList.vue
@@ -1,12 +1,36 @@
 <!-- 模板用 script -->
 <script>
-  import Card from '@/components/04_UI_Component/Card.vue';
-  
-  export default {
-    components: {
-      Card
-    },
-  };
+  import { onMounted, reactive, ref } from 'vue';
+import axios from 'axios';
+
+
+const runkey = 'c911c55c8e4f22c1ef5066573f7f0585';
+export default {
+  setup() {
+    const cardList = reactive({data:[]});
+    
+    onMounted(() => {
+      axios.get('api/productindex',{
+        headers: {
+          runkey: runkey,
+        },
+        params: {
+          category: null,
+          count: 10,
+          page: 1
+        }
+      })
+        .then(response =>{
+          cardList.data = response.data.product;
+          console.log(cardList.data);
+        })
+        .catch (error => {
+          console.log(error);
+        })
+    });
+    return {cardList};
+  }
+}
 </script>
 
 
@@ -15,13 +39,38 @@
   <section class="project-list_section">
     <div class="container">
       <div class="project-list_content">
-
+        <!-- <h2>{{ cardList.data }}</h2> -->
         <!-- card-content -->
         <div class="row">
-          <!-- card -1 -->
-          <Card />
-          <!-- card -2 -->
-          <Card />
+
+          <div class="card-content col-12 col-sm-6 col-xl-4" v-for="item in cardList.data" :key="item.id">
+            <a href="#" class="card po-relative" >
+              <div class="card-img" style="position:relative;">
+                <img src="~@/assets/image/portfolio/port1.jpg" alt="">
+                <!-- 有折數回傳顯示 -->
+                <div class="po-absolute tag discont text-500" style="right: 24px;bottom: 24px;">25</div>
+              </div>
+              <div class="card-body">
+                <p class="card-tag-text text-bold">珠寶首飾 JEWELRY</p>
+                <h3 class="card-title mb-16">{{ item.name }}</h3>
+                <p class="card-text multiline-ellipsis">
+                  {{ item.description }}
+                </p>
+              </div>
+              <div class="card-footer">
+                <!-- card - 文字 -->
+                <div class="d-flex justify-content-between align-items-end">
+                  <div class="card-text-icon-wrap">
+                    <span class="material-symbols-outlined">
+                      sell
+                    </span>
+                    <p class="card-text line-thought">{{ item.market_price }}</p>
+                  </div>
+                  <p class="text-2xl text-bold">{{ item.price }}</p>
+                </div>
+              </div>
+            </a>
+          </div>
           
           
         </div>
@@ -81,4 +130,48 @@
   .tag-text {
     @include text-base;
   }
+  /*//////////////////////////
+===== component - card =====
+//////////////////////////*/
+
+.card {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: $color-white;
+  border-radius: 8px;
+  overflow: hidden;
+}
+.card-body {
+  flex: 1 1 auto;
+}
+.card-img {
+  // aspect-ratio: 1.618 / 1;
+  aspect-ratio: 1 / 1;
+  overflow: hidden
+}
+.card-img img {
+  object-fit: cover;
+  object-position: 50% 50%;
+}
+.card-title {
+  @include text-xl;
+}
+.card-subtitle {
+  @include text-base;
+}
+.card-text {
+  @include text-sm;
+}
+.card-tag-text {
+  @include text-base;
+}
+.card-text-icon-wrap {
+  display: flex;
+  align-items: center;
+}
+.line-thought {
+  text-decoration-line: line-through;
+  text-decoration-color: $color-text-main;
+}
 </style>
